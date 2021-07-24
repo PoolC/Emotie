9b6f01ba4033430b24e34b548c57f374abf4d294
Gloria <ippnsj@gmail.com>
2021-07-24 16:59:56 +0900

    setting EMOTIONS

---
+++ src/utils/Emotions.js
@@ -0,0 +1,10 @@
+const HAPPY = {color:"#FFF27D", tag:"기쁨"};
+const ANGRY = {color:"#FF855E", tag:"화남"};
+const SAD = {color:"#9FA7EF", tag:"슬픔"};
+const SURPRISED = {color:"#AEE477", tag:"놀람"};
+const JEALOUS = {color:"#9431A4", tag:"질투"};
+const FLUTTER = {color:"#F29CB6", tag:"설렘"};
+const NORMAL = {color:"#FFFFFF", tag:"무난"};
+const TIRED = {color:"#ADADAD", tag:"지침"};
+
+export default {HAPPY, ANGRY, SAD, SURPRISED, JEALOUS, FLUTTER, NORMAL, TIRED};
