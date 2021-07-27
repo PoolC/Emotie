9f080d8327b139f2f3f1fc164a9349508a5040da
Gloria <ippnsj@gmail.com>
2021-07-27 19:25:31 +0900

    Add RecommendPage

--- src/component/pages/RecommendPage/index.js
+++ src/component/pages/RecommendPage/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-import { Container } from "./style";
+import { Container, ProfileList } from "./style";
 import Header from "../../common/Header";
+import ProfileCard from "../../common/ProfileCard";
+import FloatingButton from "../../common/FloatingButton";
+import { GiPencil } from "react-icons/gi";
+
+const emotions = [{color:"#FFF27D", tag:"기쁨"}, {color:"#FF855E", tag:"화남"}, {color:"#9FA7EF", tag:"슬픔"}, {color:"#AEE477", tag:"놀람"}, {color:"#9431A4", tag:"질투"}, {color:"#F29CB6", tag:"설렘"}, {color:"#FFFFFF", tag:"무난"}, {color:"#ADADAD", tag:"지침"}];
 
 function RecommendPage(props) {
+    const profiles =  emotions.map((emotion, index) => 
+        <ProfileCard key={index} emotion={emotion}/>
+    );
+
     return (
         <Container>
             <Header search/>
-            RecommendPage
+            <ProfileList>
+                {profiles}
+            </ProfileList>
+            <FloatingButton icon={GiPencil}/>
         </Container>
     );
 }
