f32c168c3da838d1fb1257715f41cf1b89b7a8bf
Gloria <ippnsj@gmail.com>
2021-07-27 18:13:23 +0900

    Add FeedPage

--- src/component/pages/FeedPage/style.js
+++ src/component/pages/FeedPage/style.js
@@ -4,6 +4,23 @@ export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    padding: 90px 30px 30px 30px;
+    padding: 80px 30px 20px 30px;
     box-sizing: border-box;
+
+    @media only screen and (max-width: 768px) {
+        padding: 75px 15px 15px 15px;
+    }
+`
+
+export const PostList = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    width: 60%;
+    gap: 20px;
+
+    @media only screen and (max-width: 768px) {
+        width: 100%;
+        gap: 15px;
+    }
 `
