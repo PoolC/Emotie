ade45809d7eb9c2cf038b0764f4b03f17beedc5b
phj <mumwa@naver.com>
2021-08-07 04:54:31 +0900

    add login page

--- src/component/pages/LoginPage/index.js
+++ src/component/pages/LoginPage/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Container } from "./style";
+import { useParams } from "react-router-dom";
+
 import Header from "../../common/Header";
+import logo from "../../../image/logo_img.svg";
+import PillButton from "../../common/PillButton";
+import PillInput from "../../common/PillInput";
+
+
+import { 
+    Container, Title, Text, Logo, Switch, ButtonFallback
+} from "./style";
+
+const inputprops = ["이메일", "비밀번호"];
 
 function LoginPage(props) {
+    const input = inputprops.map((value, index)=>
+        <PillInput width="200px" key={index} placeholder={value}></PillInput>
+    );
     return (
         <Container>
             <Header/>
-            LoginPage
+            <Logo src={logo}></Logo>
+            <Title>Emotie 로그인</Title>
+            <Text>Emotie에 오신 걸 환영합니다</Text>
+            {input}
+            <PillButton width="260px">로그인</PillButton>
+            <ButtonFallback>비밀번호를 잊으셨나요?</ButtonFallback>
+            <Switch href="">계정이 없으신가요? 가입하기</Switch>
         </Container>
     );
 }
--- src/component/pages/LoginPage/style.js
+++ src/component/pages/LoginPage/style.js
@@ -4,10 +4,80 @@ export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    padding: 80px 30px 20px 30px;
+    padding: 110px 30px 20px 30px;
     box-sizing: border-box;
+    gap:20px;
 
     @media only screen and (max-width: 768px) {
-        padding: 75px 15px 15px 15px;
+        padding: 90px 15px 15px 15px;
+        gap:10px;
+    }
+`
+export const Gap = styled.div`
+    gap:15px;
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const ButtonFallback = styled.a`
+    width:300px;
+    height:20px;
+    font-size: 10pt;
+    text-align:center;
+    text-decoration:none;
+    color:#FFFFFF;
+    margin-top:-10px;
+    :hover{
+        text-decoration:underline;
+    }
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const Title = styled.div`
+    width: 400px;
+    height: 40px;
+    font-size: 24pt;
+    text-align:center;
+    font-weight:bold;
+    color:#FFFFFF;
+
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const Text = styled.div`
+    width:400px;
+    height:60px;
+    font-size: 12pt;
+    text-align:center;
+    color:#FFFFFF;
+
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const Switch = styled.a`
+    border-top:1px solid #FFFFFF;
+    width:400px;
+    height:40px;
+    margin:30px;
+    font-size:10pt;
+    color:#FFFFFF;
+    text-decoration:none;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    :hover{
+        text-decoration:underline;
+    }
+    @media only screen and (max-width: 768px) {
+    }
+    
+`
+
+export const Logo = styled.img`
+    width:50px;
+    @media only screen and (max-width: 768px) {
     }
 `
