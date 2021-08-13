5a42fc5201e5340e763c6444f9a7cf7b9c656b9b
phj <mumwa@naver.com>
2021-08-14 00:22:42 +0900

    Add FindPage

--- src/component/pages/FindPage/index.js
+++ src/component/pages/FindPage/index.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Container } from "./style";
+import { useParams } from "react-router-dom";
+
 import Header from "../../common/Header";
+import PillButton from "../../common/PillButton";
+import PillInput from "../../common/PillInput";
 
+
+import {
+    Container, Title, Text, FlexBox, CertButton, Gap
+} from "./style";
 function FindPage(props) {
+    const [step, setStep] = useState(0);
+    const cert = "인증번호 받기";
+    let Button = null;
+    if (step === 0) {
+        Button = <>
+        <Title>계정 찾기</Title>
+        <Text>이메일을 입력하세요</Text>
+        <PillInput width="200px" placeholder="이메일"></PillInput>
+        <PillButton width="260px" onClick={() => setStep(1)}>검색</PillButton>
+        </>;
+    } else if (step === 1) {
+        Button =
+        <>
+        <Title>이메일 인증</Title>
+        <Text>이메일을 인증하고 비밀번호를 변경하세요</Text>
+        <FlexBox>
+            <PillInput width='140px' placeholder='이메일'></PillInput><CertButton children={cert}></CertButton>
+        </FlexBox>
+        <PillInput width="200px" placeholder="이메일 인증번호"></PillInput>
+        <FlexBox>
+            <PillButton negative width="120px" onClick={() => setStep(0)}>이전</PillButton>
+            <PillButton width="120px" onClick={() => setStep(2)}>다음</PillButton>
+        </FlexBox>
+        </>;
+    } else if (step === 2) {
+        Button =
+        <>
+        <Title>비밀번호 변경</Title>
+        <Text>변경할 비밀 번호를 입력하세요</Text>
+        <PillInput width="200px" placeholder="새 비밀번호"></PillInput>
+        <PillInput width="200px" placeholder="비밀번호 재입력"></PillInput>
+        <FlexBox>
+            <PillButton negative width="120px" onClick={() => setStep(1)}>이전</PillButton>
+            <PillButton width="120px" onClick={() => setStep(3)}>확인</PillButton>
+        </FlexBox>
+        </>;
+    } else if (step === 3) {
+        Button = 
+        <>
+        <Title>비밀번호 변경 완료</Title>
+        <Text>비밀번호 변경이 완료되었습니다</Text>;
+        <PillButton width="260px">로그인 페이지로</PillButton>
+        </>;
+    } else {
+        Button = <Text>잘못된 접근입니다.</Text>;
+    }
     return (
         <Container>
             <Header/>
-            FindPage
+                {Button}
         </Container>
     );
 }
--- src/component/pages/FindPage/style.js
+++ src/component/pages/FindPage/style.js
@@ -4,10 +4,78 @@ export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    padding: 80px 30px 20px 30px;
+    padding: 110px 30px 20px 30px;
     box-sizing: border-box;
+    gap:20px;
 
     @media only screen and (max-width: 768px) {
-        padding: 75px 15px 15px 15px;
+        padding: 90px 15px 15px 15px;
+        gap:10px;
+    }
+`
+
+export const ButtonText = styled.a`
+    width:300px;
+    height:20px;
+    font-size: 10pt;
+    text-align:center;
+    text-decoration:none;
+    color:#FFFFFF;
+    margin-top:-10px;
+    :hover{
+        text-decoration:underline;
+    }
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const Title = styled.div`
+    width: 400px;
+    height: 40px;
+    font-size: 24pt;
+    text-align:center;
+    font-weight:bold;
+    color:#FFFFFF;
+
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const Text = styled.div`
+    width:400px;
+    height:60px;
+    font-size: 12pt;
+    text-align:center;
+    color:#FFFFFF;
+
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const FlexBox = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    color:#ffffff;
+    font-size:8pt;
+    @media only screen and (max-width: 768px) {
+    }
+`
+
+export const CertButton = styled.button`
+    margin-left:10px;  
+    width: 50px;
+    height: 40px;
+    border-radius: 10px;
+    background-color: #707070;
+    border: none;
+    color: #ffffff;
+    font-size:3pt;
+    transition: border 300ms, opacity 300ms;
+    &:hover {
+        background-color: #A7A7A7;
+    }
+    &:active {
+        opacity: 0.6;
     }
 `
