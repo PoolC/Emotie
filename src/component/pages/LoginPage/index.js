8f1b76b05df9c58f0838cb5ae9efa2c68b5bd0b9
mumwa <mumwa@naver.com>
2021-08-20 18:05:35 +0900

    Add LoginPage input detect

--- src/component/pages/LoginPage/index.js
+++ src/component/pages/LoginPage/index.js
@@ -12,12 +12,33 @@ import {
     Container, Title, Text, Logo, Switch, ButtonText
 } from "./style";
 
-const inputprops = ["이메일", "비밀번호"];
-
 function LoginPage(props) {
     const goFindPage = () => props.history.push('/find');
     const goRegisterPage = () => props.history.push('/register');
     const login = () => console.log('로그인');
+    const detectInput=()=>{
+        if(id.length===0){
+            alert('이메일을 입력하세요');
+            return;
+        }if(password.length===0){
+            alert('패스워드를 입력하세요');
+            return;
+        }
+        login();
+    }
+
+    const [inputs, setInputs]=useState({
+        id:'',
+        password:'',
+    });
+    const {id, password}=inputs;
+    const onChange=(e)=>{
+        const{value,name}=e.target;
+        setInputs({
+            ...inputs,
+            [name]:value
+        });
+    };
 
     return (
         <Container>
@@ -25,9 +46,9 @@ function LoginPage(props) {
             <Logo src={logo}></Logo>
             <Title>Emotie 로그인</Title>
             <Text>Emotie에 오신 걸 환영합니다</Text>
-            <PillInput width="200px" placeholder="이메일" type="text"></PillInput>
-            <PillInput width="200px" placeholder="비밀번호" type="password"></PillInput>
-            <PillButton width="260px" onClick={login}>로그인</PillButton>
+            <PillInput name="id" value={id} onChange={onChange} width="200px" placeholder="이메일" type="text"></PillInput>
+            <PillInput name="password" value={password} onChange={onChange} width="200px" placeholder="비밀번호" type="password"></PillInput>
+            <PillButton width="260px" onClick={detectInput}>로그인</PillButton>
             <ButtonText onClick={goFindPage}>비밀번호를 잊으셨나요?</ButtonText>
             <Switch onClick={goRegisterPage}>계정이 없으신가요? 가입하기</Switch>
         </Container>
