f96813ec4ad1e5da50ed720ce1a5e451c4171a11
namoo <namoo.developer@gmail.com>
2021-08-28 17:29:47 +0900

    Modify server.js modify base url

--- src/utils/server.js
+++ src/utils/server.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const server = axios.create();
 // server.defaults.headers.common[''] = '';
-server.defaults.baseURL = "http://localhost:8080";
+server.defaults.baseURL = "http://3.35.248.237:8081/";
 
 export default server;
