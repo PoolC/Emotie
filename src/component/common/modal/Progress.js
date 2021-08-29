ae02d71c96507adb686ea3d417e4b9eeebad34dd
namoo <namoo.developer@gmail.com>
2021-08-29 23:31:46 +0900

    Modify Progress modal add color to progress bar and refactor codes

--- src/component/common/modal/Progress.js
+++ src/component/common/modal/Progress.js
@@ -3,16 +3,25 @@ import styled, { keyframes, css } from "styled-components";
 function Progress(props) {
     return (
         <Container isInProgress={props.isInProgress}>
-            <Bar isInProgress={props.isInProgress} delay="0s"/>
-            <Bar isInProgress={props.isInProgress} delay="100ms"/>
-            <Bar isInProgress={props.isInProgress} delay="200ms"/>
-            <Bar isInProgress={props.isInProgress} delay="300ms"/>
-            <Bar isInProgress={props.isInProgress} delay="400ms"/>
+            <Bar isInProgress={props.isInProgress} color="#FF855D" delay="0s"/>
+            <Bar isInProgress={props.isInProgress} color="#FFF27D" delay="100ms"/>
+            <Bar isInProgress={props.isInProgress} color="#AEE477" delay="200ms"/>
+            <Bar isInProgress={props.isInProgress} color="#9FA7EF" delay="300ms"/>
+            <Bar isInProgress={props.isInProgress} color="#F29CB6" delay="400ms"/>
         </Container>
     );
 }
 
 export default Progress;
+
+const stretch = keyframes`
+    0% {
+        transform: scaleY(1.0);
+    }
+    100% {
+        transform: scaleY(1.5);
+    }
+`
 
 const Container = styled.div`
     position: fixed;
@@ -24,7 +33,7 @@ const Container = styled.div`
     align-items: center;
     width: 100%;
     height: 100vh;
-    gap: 5px;
+    gap: 7px;
     z-index: 10;
     backdrop-filter: blur(3px);
     opacity: ${props => props.isInProgress ? "1" : "0"};
@@ -32,19 +41,11 @@ const Container = styled.div`
     transition: 200ms all;
     pointer-events: none;
 `
-const stretch = keyframes`
-    0% {
-        transform: scaleY(1.0);
-    }
-    100% {
-        transform: scaleY(1.5);
-    }
-`
 const Bar = styled.div`
     width: 6px;
     height: 30px;
     border-radius: 3px;
-    background-color: white;
+    background-color: ${props => props.color};
     ${props => props.isInProgress && css`
         animation: ${stretch} ease 400ms ${props.delay} infinite alternate;
     `}
