eb431d584dcf3128d4d4d8862bf9882047ba7126
mumwa <mumwa@naver.com>
2021-09-05 03:11:09 +0900

    Add detailpage

--- src/component/pages/DetailPage/index.js
+++ src/component/pages/DetailPage/index.js
@@ -1,29 +1,40 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import { Container, Profile, Post, ProfileButton} from "./style";
+import { Container, Profile, Post, ProfileButton, Wrapper, Info, Nickname, Date, Content, Icons} from "./style";
 import Header from "../../common/Header";
-import PostCard from "../../common/PostCard";
-import FloatingButton from "../../common/FloatingButton";
-import { GiPencil } from "react-icons/gi";
-import { MdKeyboardArrowUp } from "react-icons/md"
+import EmotionTag from '../../common/EmotionTag';
+import IconButton from '../../common/IconButton';
+import { AiOutlineShareAlt} from "react-icons/ai";
 
 function DetailPage(props) {
     const { id, postId } = useParams();
-
     return (
         <Container>
             <Header></Header>
             <Profile>
                 <ProfileButton children="프로필 가기>>"></ProfileButton>
             </Profile>
-            <Post>
-                <PostCard report share></PostCard>
-            </Post>
-
-            {/* DetailPage<br/>{id}의 {postId}번째 포스트 */}
+            <Wrapper>
+                <Post>
+                <Info>
+                    <EmotionTag emotion={props.emotion}/>
+                    <Nickname>{id}</Nickname>
+                    <Date>2021.07.20</Date>
+                </Info>
+                <Content>그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건지 모르겠다. 집에 가고 싶다. 그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건지 모르겠다. 집에 가고 싶다.</Content>
+                <Icons>
+                    <IconButton icon={AiOutlineShareAlt} size="1.2rem" color="#7E7E7E" onClick={onShare}/>
+                </Icons>
+                </Post>
+            </Wrapper>
         </Container>
     );
+    function onShare(event) {
+        event.stopPropagation();
+        console.log("share");
+    }
+
 }
 
 export default DetailPage;
--- src/component/pages/DetailPage/style.js
+++ src/component/pages/DetailPage/style.js
@@ -29,14 +29,19 @@ export const Profile = styled.div`
     @media only screen and (max-width: 768px) {
     }
 `
-export const ProfileButton = styled.div`
+export const ProfileButton = styled.a`
     padding:10px;
+    font-size:0.8em;
     font-weight:bold;
     width:60%;
     text-align:right;
     margin:0 20%;
+    &:hover {
+        cursor:pointer;
+    }
+    
 `
-export const Post= styled.div`
+export const Wrapper= styled.div`
     display: flex;
     flex-flow: column nowrap;
     height: 40vh;
@@ -49,5 +54,57 @@ export const Post= styled.div`
 
     @media only screen and (max-width: 768px) {
         padding: 15px 15px 15px 15px;
+    }
+`
+export const Post = styled.div`
+    width:100%;
+    height:30vh;
+    margin:30px 0;
+
+`
+export const Info = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    width: 100%;
+    margin:15px 0;
+`
+
+export const Nickname = styled.span`
+    flex: 1 0 0;
+    margin-left: 20px;
+    font-size: 0.9rem;
+    font-weight: bold;
+    color: #ffffff;
+`
+
+export const Date = styled.span`
+    font-size: 0.6rem;
+    color: #ffffff;
+`
+
+export const Content = styled.p`
+    display: -webkit-box;
+    margin: 0;
+    width: 70%;
+    font-size: 0.6rem;
+    color: #ffffff;
+    line-height: 2;
+    height: 6em;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical; 
+    word-wrap: break-word;
+    -webkit-line-clamp: 3;
+`
+
+export const Icons = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    gap: 10px;
+
+    @media only screen and (max-width: 1100px) {
+        display: none;
     }
 `
