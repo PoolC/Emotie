f763987a87abcca737decaf25c9943924e2b1368
mumwa <mumwa@naver.com>
2021-09-04 22:39:17 +0900

    Add detailpage frame

--- src/component/pages/DetailPage/index.js
+++ src/component/pages/DetailPage/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import { Container } from "./style";
+import { Container, Profile, Post, ProfileButton} from "./style";
+import Header from "../../common/Header";
+import PostCard from "../../common/PostCard";
+import FloatingButton from "../../common/FloatingButton";
+import { GiPencil } from "react-icons/gi";
+import { MdKeyboardArrowUp } from "react-icons/md"
 
 function DetailPage(props) {
     const { id, postId } = useParams();
 
     return (
         <Container>
-            DetailPage<br/>{id}의 {postId}번째 포스트
+            <Header></Header>
+            <Profile>
+                <ProfileButton children="프로필 가기>>"></ProfileButton>
+            </Profile>
+            <Post>
+                <PostCard report share></PostCard>
+            </Post>
+
+            {/* DetailPage<br/>{id}의 {postId}번째 포스트 */}
         </Container>
     );
 }
--- src/component/pages/DetailPage/style.js
+++ src/component/pages/DetailPage/style.js
@@ -4,11 +4,48 @@ export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
     height: 100vh;
-    justify-content: center;
     align-items: center;
-    padding: 20px 30px 20px 30px;
+    padding: 60px 0 0 0;
     box-sizing: border-box;
     color: white;
+
+    @media only screen and (max-width: 768px) {
+        padding: 15px 15px 15px 15px;
+    }
+`
+
+export const Profile = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    height: 20vh;
+    align-items: center;
+    margin:0;
+    width:100%;
+    box-sizing: border-box;
+    color: #1E1E1E;
+    background-color:#9431A4;
+    flex-direction: column-reverse;
+
+    @media only screen and (max-width: 768px) {
+    }
+`
+export const ProfileButton = styled.div`
+    padding:10px;
+    font-weight:bold;
+    width:60%;
+    text-align:right;
+    margin:0 20%;
+`
+export const Post= styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    height: 40vh;
+    width:60%;
+    align-items: center;
+    margin:0 20%;
+    box-sizing: border-box;
+    color: black;
+    background-color:#1E1E1E;
 
     @media only screen and (max-width: 768px) {
         padding: 15px 15px 15px 15px;
