dd15701173835b50d90aa445340a9dd7b8381844
mumwa <mumwa@naver.com>
2021-09-10 18:47:07 +0900

    Modify layout

--- src/component/pages/DetailPage/index.js
+++ src/component/pages/DetailPage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import { Container, Profile, Post, ProfileButton, Wrapper, Info, Nickname, Date, Content, Icons} from "./style";
+import { Container, Profile, Post, ProfileWrapper, ProfileButton, Wrapper, Info, Nickname, Date, Content} from "./style";
 import Header from "../../common/Header";
 import EmotionTag from '../../common/EmotionTag';
 import IconButton from '../../common/IconButton';
@@ -13,7 +13,9 @@ function DetailPage(props) {
         <Container>
             <Header></Header>
             <Profile>
-                <ProfileButton children="프로필 가기>>"></ProfileButton>
+                <ProfileWrapper>
+                    <ProfileButton children="프로필 가기"></ProfileButton>
+                </ProfileWrapper>
             </Profile>
             <Wrapper>
                 <Post>
@@ -22,10 +24,8 @@ function DetailPage(props) {
                         <Nickname>{id}</Nickname>
                         <Date>2021.07.20</Date>
                     </Info>
-                    <Content>그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건지 모르겠다. 집에 가고 싶다. 그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건지 모르겠다. 집에 가고 싶다.</Content>
-                    <Icons>
-                        <IconButton icon={AiOutlineShareAlt} size="1.2rem" color="#7E7E7E" onClick={onShare}/>
-                    </Icons>
+                    <Content>그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건.그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건.그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건.그 자식한테 화가 나는 건지 나 자신한테 화가 나는건지 잘 모르겠다. 내가 뭘 잘못 했다고 나한테 이런 일이 일어나는 건.</Content>
+                    <IconButton icon={AiOutlineShareAlt} size="1.2rem" color="#7E7E7E" onClick={onShare}/>
                 </Post>
             </Wrapper>
         </Container>
--- src/component/pages/DetailPage/style.js
+++ src/component/pages/DetailPage/style.js
@@ -3,76 +3,83 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
-    height: 100vh;
     align-items: center;
-    padding: 60px 0 0 0;
+    padding: 60px 0 30px 0;
     box-sizing: border-box;
     color: white;
-
-    @media only screen and (max-width: 768px) {
-        padding: 15px 15px 15px 15px;
-    }
 `
 
 export const Profile = styled.div`
     display: flex;
     flex-flow: column nowrap;
-    height: 20vh;
-    align-items: center;
     margin:0;
     width:100%;
+    height:200px;
     box-sizing: border-box;
     color: #1E1E1E;
-    background-color:#9431A4;
+    background-color:#FFF27D;
     flex-direction: column-reverse;
-
-    @media only screen and (max-width: 768px) {
-    }
-`
-export const ProfileButton = styled.a`
-    padding:10px;
-    font-size:1em;
-    font-weight:bold;
-    width:60%;
-    text-align:right;
-    margin:0 20%;
     &:hover {
         cursor:pointer;
     }
-    
+`
+export const ProfileWrapper = styled.div`
+    position:relative;
+    width:60%;
+    margin:0 20%;
+    @media only screen and (max-width: 768px) {
+        width:100%;
+        margin:0;
+    }
+`
+export const ProfileButton = styled.a`
+    position:absolute;
+    padding:10px;
+    bottom:20px;
+    right:20px;
+    color:white;
+    font-size:1em;
+    font-weight:bold;
+    border-radius:15px;
+    background-color:#1E1E1E;
 `
 export const Wrapper= styled.div`
     display: flex;
     flex-flow: column nowrap;
-    height: 40vh;
     width:60%;
     align-items: center;
-    margin:0 20%;
     box-sizing: border-box;
     color: black;
     background-color:#1E1E1E;
-
     @media only screen and (max-width: 768px) {
-        padding: 15px 15px 15px 15px;
+        width:80%;
     }
 `
 export const Post = styled.div`
     width:100%;
-    height:30vh;
-    margin:30px 0;
-
+    margin:5% 0;
+    display:flex;
+    flex-flow: column nowrap;
+    align-items:flex-end;
+    gap:10px;
 `
 export const Info = styled.div`
+    gap:15px;
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
     width: 100%;
     margin:15px 0;
+    @media only screen and (max-width: 768px) {
+        gap:8px;
+        flex-flow:column nowrap;
+        align-items:flex-start;
+    }
+
 `
 
 export const Nickname = styled.span`
     flex: 1 0 0;
-    margin-left: 20px;
     font-size: 1rem;
     font-weight: bold;
     color: #ffffff;
@@ -84,27 +91,10 @@ export const Date = styled.span`
 `
 
 export const Content = styled.p`
-    display: -webkit-box;
     margin: 0;
-    width: 70%;
+    width: 100%;
     font-size: 1rem;
     color: #ffffff;
     line-height: 2;
-    height: 6em;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    -webkit-box-orient: vertical; 
     word-wrap: break-word;
-    -webkit-line-clamp: 3;
-`
-
-export const Icons = styled.div`
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: flex-end;
-    gap: 10px;
-
-    @media only screen and (max-width: 1100px) {
-        display: none;
-    }
 `
