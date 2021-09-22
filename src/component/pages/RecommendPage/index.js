ef9c6a7a9db935a1d8c8605aa90ca3d230e1f800
Gloria <ippnsj@gmail.com>
2021-09-22 20:02:56 +0900

    Fix RecommendPage import and add modal

--- src/component/pages/RecommendPage/index.js
+++ src/component/pages/RecommendPage/index.js
@@ -5,6 +5,9 @@ import Header from "../../common/Header";
 import ProfileCard from "../../common/ProfileCard";
 import FloatingButton from "../../common/FloatingButton";
 import { GiPencil } from "react-icons/gi";
+import Progress from "../../common/modal/Progress";
+
+import server from "../../../utils/server";
 
 const emotions = [{color:"#FFF27D", tag:"기쁨"}, {color:"#FF855E", tag:"화남"}, {color:"#9FA7EF", tag:"슬픔"}, {color:"#AEE477", tag:"놀람"}, {color:"#9431A4", tag:"질투"}, {color:"#F29CB6", tag:"설렘"}, {color:"#FFFFFF", tag:"무난"}, {color:"#ADADAD", tag:"지침"}];
 
@@ -39,6 +42,8 @@ function RecommendPage(props) {
                 {profiles}
             </ProfileList>
             <FloatingButton icon={GiPencil} onClick={goWritePage}/>
+            {/* 모달 */}
+            <Progress isInProgress={loading} fullscreen={fullscreen}/>
         </Container>
     );
 }
