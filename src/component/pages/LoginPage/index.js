2c7dc590f4a4467f0f4d3b988e32de795a246857
mumwa <mumwa@naver.com>
2021-10-01 17:51:10 +0900

    Fix login dispatch

--- src/component/pages/LoginPage/index.js
+++ src/component/pages/LoginPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import * as saga from "../../../store/actions/_saga";
 
@@ -8,8 +8,6 @@ import logo from "../../../image/logo_img.svg";
 import PillButton from "../../common/PillButton";
 import PillInput from "../../common/PillInput";
 import Alert from "../../common/modal/Alert"
-
-import * as api from "../../../utils/api";
 
 
 import {
@@ -63,44 +61,39 @@ function LoginPage(props) {
         login();
     }
     const login = () => {
-        console.log('로그인');
-        api.follow(id, password)
-            .then(() => {
-                const payload = {
-                    email: id,
-                    password: password
-                }
-                dispatch(saga.login(payload));
-            }
-            )
-            .catch(error => {
-                if (error.response) {
-                    // 요청이 이루어졌으나 서버가 2xx의 범위를 벗어나는 상태 코드
-                    if (error.response && error.response.status === 401) {
-                        setAlertTitle(error.response.status);
-                        setAlertMsg('비밀번호가 틀렸습니다.');
-                        setOpen(true);
-                    } else if (error.response && error.response.status === 404) {
-                        setAlertTitle(error.response.status);
-                        setAlertMsg('가입되지 않은 이메일입니다.');
-                        setOpen(true);
-                    } else {
-                        setAlertTitle(error.response.status);
-                        setAlertMsg('알 수 없는 에러가 발생했습니다.');
-                        setOpen(true);
-                    }
-                }
-                else if (error.request) {
-                    setAlertTitle('에러');
-                    setAlertMsg('서버에서 응답이 오지 않습니다.')
-                    setOpen(true);
-                }
-                else {
-                    setAlertTitle('에러');
-                    setAlertMsg('로그인 요청에 문제가 발생했습니다')
-                    setOpen(true);
-                }
-            });
+        const payload = {
+            email: id,
+            password: password
+        }
+        dispatch(saga.login(payload))
+            // .catch(error => {
+            //     if (error.response) {
+            //         // 요청이 이루어졌으나 서버가 2xx의 범위를 벗어나는 상태 코드
+            //         if (error.response.status === 401) {
+            //             setAlertTitle(error.response.status);
+            //             setAlertMsg('비밀번호가 틀렸습니다.');
+            //             setOpen(true);
+            //         } else if (error.response.status === 404) {
+            //             setAlertTitle(error.response.status);
+            //             setAlertMsg('가입되지 않은 이메일입니다.');
+            //             setOpen(true);
+            //         } else {
+            //             setAlertTitle(error.response.status);
+            //             setAlertMsg('알 수 없는 에러가 발생했습니다.');
+            //             setOpen(true);
+            //         }
+            //     }
+            //     else if (error.request) {
+            //         setAlertTitle('에러');
+            //         setAlertMsg('서버에서 응답이 오지 않습니다.')
+            //         setOpen(true);
+            //     }
+            //     else {
+            //         setAlertTitle('에러');
+            //         setAlertMsg('로그인 요청에 문제가 발생했습니다')
+            //         setOpen(true);
+            //     }
+            // });
     }
 
     return (
