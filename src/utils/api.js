c975aab847201bcae94706b967ade845aec95ce3
Gloria <ippnsj@gmail.com>
2021-10-08 16:10:57 +0900

    Refactor api.js apply edited api urls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,41 +1,32 @@
 import server from "./server";
 
-// 가입
+// 회원
 export const register = (nickname, password, rePassword, gender, dateOfBirth, email) => server.post('/members', {nickname: nickname, password: password, passwordCheck: rePassword, gender: gender, dateOfBirth: dateOfBirth, email: email});
+export const getUserInfo = () => server.get('/members/me');
+export const checkNicknameDuplicated = (nickname) => server.get('/members/nickname', { nickname: nickname });
+export const updateUserInfo = (nickname, gender, birth) => server.put('/members', { nickname: nickname, gender: gender, dateOfBirth: birth });
+export const changePassword = (currentPassword, password, passwordCheck) => server.put('/members/password', { currentPassword: currentPassword, password: password, passwordCheck: passwordCheck });
+export const deleteAccount = (currentPassword, reason) => server.delete('/members', { currentPassword: currentPassword, reason: reason });
+export const follow = (uuid, state) => server.post(`/members/follow/${uuid}`, { isFollowing: state });
 
 // 인증
 export const pwResetEmail = (email) => server.post(`/auth/password-reset?email=${email}`);
 export const pwResetCheck = (token, email, password, passwordCheck) => server.put(`/auth/password-reset?passwordRestToken=${token}`, {email: email, password: password, passwordCheck: passwordCheck});
 export const activateAccount = (email, token) => server.put(`/auth/authorization?email=${email}&authorizationToken=${token}`);
-
-// 로그인
 export const login = ({ email, password }) => server.post('/auth/login', { email: email, password: password });
-export const getUserInfo = () => server.get('/members/me');
+
+// 다이어리
+export const uploadPost = (emotion, content, isPrivate) => server.post('/diaries', { emotion: emotion, content: content, isOpened:  isPrivate });
+export const blur = (id) => server.post(`/diaries/blind/${id}`); // api 구현 후 수정 필요
+export const reportDiary = (id) => server.post(`/diaries/report/${id}`);
+export const deleteDiary = (id) => server.delete('/diaries', { id: [id] });
+export const getFeeds = (pageNumber) => server.get(`/feed?page=${pageNumber}`);
+
+// 방명록
+export const reportGuestbook = (id) => server.post(`/guestbooks/report/${id}`);
+export const deleteGuestbook = (id) => server.delete(`/guestbooks/${id}`);
 
 // 프로필
 export const getProfileInfo = (uuid) => server.get(`/profiles/${uuid}`);
-export const editNickname = (nickname) => server.put('/profiles', { nickname: nickname });
 export const editIntroduction = (introduction) => server.put('/profiles', { introduction: introduction });
-
-// 설정
-export const checkNicknameDuplicated = (nickname) => server.get('/members/nickname', { nickname: nickname });
-export const updateUserInfo = (nickname, gender, birth) => server.put('/members', { nickname: nickname, gender: gender, dateOfBirth: birth });
-export const changePassword = (currentPassword, password, passwordCheck) => server.put('/members/password', { currentPassword: currentPassword, password: password, passwordCheck: passwordCheck });
-export const deleteAccount = (currentPassword, reason) => server.delete('/members', { currentPassword: currentPassword, reason: reason });
-
-// 구독
-export const follow = (uuid, state) => server.post(`/members/follow/${uuid}`, { isFollowing: state });
-
-// 불러오기
 export const getRecommends = () => server.get('/recommend');
-export const getFeeds = () => server.get('/feed');
-
-// 등록
-export const uploadPost = (emotion, content, isPrivate) => server.post('/diaries', { emotion: emotion, content: content, isOpened:  isPrivate });
-
-// 다이어리 및 방명록 관리
-export const blur = (id) => server.post(`/diaries/blind/${id}`);
-export const reportDiary = (id) => server.post(`/diaries/report/${id}`);
-export const reportGuestbook = (id) => server.post(`/guestbooks/report/${id}`);
-export const deleteDiary = (id) => server.delete('/diaries', { id: [id] });
-export const deleteGuestbook = (id) => server.delete(`/guestbooks/${id}`);
