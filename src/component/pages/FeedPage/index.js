c7fa5cc6a99fba783efa5515bcfae52d7b690898
Gloria <ippnsj@gmail.com>
2021-10-29 16:32:47 +0900

    Modify FeedPage use localstorage to save state

--- src/component/pages/FeedPage/index.js
+++ src/component/pages/FeedPage/index.js
@@ -14,10 +14,14 @@ import * as api from "../../../utils/api";
 const emotions = [{color:"#FFF27D", tag:"기쁨"}, {color:"#FF855E", tag:"화남"}, {color:"#9FA7EF", tag:"슬픔"}, {color:"#AEE477", tag:"놀람"}, {color:"#9431A4", tag:"질투"}, {color:"#F29CB6", tag:"설렘"}, {color:"#FFFFFF", tag:"무난"}, {color:"#ADADAD", tag:"지침"}];
 
 function FeedPage(props) {
+    window.onbeforeunload = function (e) {
+        window.localStorage.clear();
+    };
+
     const [loading, setLoading] = useState(false);
     const [fullscreen, setFullscreen] = useState(false);
-    // const [feeds, setFeeds] = useState(null);
-    const [ page, setPage ] = useState(0);
+    // const [feeds, setFeeds] = useState(() => JSON.parse(window.localStorage.getItem("feeds")) || []);
+    const [ page, setPage ] = useState(() => JSON.parse(window.localStorage.getItem("page")) || 1);
 
     const goDetailPage = () => props.history.push('/profile/:1/post/:1');
     const goToTop = () => window.scrollTo(0, 0);
@@ -27,23 +31,30 @@ function FeedPage(props) {
         <PostCard key={index} emotion={emotion} share blur report onClick={goDetailPage} category="diary" id={index}/>
     );
 
-    // async function fetchFeeds() {
-    //     setLoading(true);
-    //     setFullscreen(true);
-    //     const response = await api.getFeeds(page);
-    //     setFeeds(response.data);
-    //     setLoading(false);
-    //     setFullscreen(false);
-    // }
-    // useEffect(() => {
-    //     setPage(1);
-    //     fetchFeeds();
-    // }, []);
-
+    async function fetchFeeds() {
+        setLoading(true);
+        setFullscreen(true);
+        console.log(page);
+        // const response = await api.getFeeds(page);
+        // setFeeds(response.data);
+        setLoading(false);
+        setFullscreen(false);
+    }
+    useEffect(() => {
+        if(page === 1) {
+            fetchFeeds();
+        }
+    }, []);
+    useEffect(() => {
+        window.localStorage.setItem("page", JSON.stringify(page));
+        // window.localStorage.setItem("feeds", JSON.stringify(feeds));
+    }, [page, feeds]);
+    
     async function detectScroll() {
         setLoading(true);
         setFullscreen(false);
-        // setPage(page + 1);
+        setPage(page + 1);
+        console.log(page);
         // const response = await api.getFeeds(page);
         // setFeeds([...feeds, response.data]);
         // setLoading(false);
