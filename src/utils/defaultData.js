10c10655c61bc19562fe34a175ab24ad74de1802
namoo <namoo.developer@gmail.com>
2021-11-12 17:28:00 +0900

    Modify data update default data as empty

--- src/utils/defaultData.js
+++ src/utils/defaultData.js
@@ -1,137 +1,32 @@
 export const profileInfo = {
-    nickname: 'Test',
-    introduction: 'Test 계정입니다.',
+    nickname: '',
+    introduction: '',
     allEmotion: {
-        tag: "슬픔",
-        color: "#9FA7EF"
+        tag: "무난",
+        color: "#FFFFFF"
     },
     recentEmotion: [
         {
-            tag: "슬픔",
-            color: "#9FA7EF"
+            tag: "무난",
+            color: "#FFFFFF"
         },
         {
-            tag: "슬픔",
-            color: "#9FA7EF"
+            tag: "무난",
+            color: "#FFFFFF"
         }
     ],
     followed: false,
-    followers: [
-        {
-            memberId: '2',
-            nickname: 'Test2'
-        },
-        {
-            memberId: '3',
-            nickname: 'Test3'
-        }
-    ],
-    followees: [
-        {
-            memberId: '2',
-            nickname: 'Test2'
-        },
-        {
-            memberId: '3',
-            nickname: 'Test3'
-        }
-    ],
+    followers: [],
+    followees: [],
     // 임시
     motie: null,
     motieItems: null
 };
 
 export const diaries = {
-    diaries: [
-        {
-            diaryId: 1,
-            nickname: "Test",
-            date: "2021-07-24 21:09:02",
-            emotion: {
-                tag: "기쁨",
-                color: "#FFF27D"
-            },
-            content: "오늘 잠을 잘 잤다. 기뻤다.",
-            isOpened: true
-        },
-        {
-            diaryId: 2,
-            nickname: "Test",
-            date: "2021-07-24 21:09:02",
-            emotion: {
-                tag: "기쁨",
-                color: "#FFF27D"
-            },
-            content: "오늘 잠을 잘 잤다. 기뻤다.",
-            isOpened: true
-        },
-        {
-            diaryId: 3,
-            nickname: "Test",
-            date: "2021-07-24 21:09:02",
-            emotion: {
-                tag: "기쁨",
-                color: "#FFF27D"
-            },
-            content: "오늘 잠을 잘 잤다. 기뻤다.",
-            isOpened: true
-        },
-        {
-            diaryId: 4,
-            nickname: "Test",
-            date: "2021-07-24 21:09:02",
-            emotion: {
-                tag: "기쁨",
-                color: "#FFF27D"
-            },
-            content: "오늘 잠을 잘 잤다. 기뻤다.",
-            isOpened: true
-        },
-        {
-            diaryId: 5,
-            nickname: "Test",
-            date: "2021-07-24 21:09:02",
-            emotion: {
-                tag: "기쁨",
-                color: "#FFF27D"
-            },
-            content: "오늘 잠을 잘 잤다. 기뻤다.",
-            isOpened: true
-        },
-    ],
+    diaries: [],
 };
 
 export const guestbooks = {
-    guestbooks: [
-        {
-            guestbookId: 1,
-            nickname: "Test2",
-            date: "2021-07-24 21:09:02",
-            content: "안녕하세요"
-        },
-        {
-            guestbookId: 2,
-            nickname: "Test3",
-            date: "2021-07-24 21:09:02",
-            content: "저희 일촌 맺어요~~"
-        },
-        {
-            guestbookId: 3,
-            nickname: "Test4",
-            date: "2021-07-24 21:09:02",
-            content: "광고"
-        },
-        {
-            guestbookId: 4,
-            nickname: "Test5",
-            date: "2021-07-24 21:09:02",
-            content: "^^"
-        },
-        {
-            guestbookId: 5,
-            nickname: "Test6",
-            date: "2021-07-24 21:09:02",
-            content: "안녕하세요"
-        },
-    ],
+    guestbooks: [],
 };
