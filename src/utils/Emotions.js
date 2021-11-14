4cb5f1f4304eee3e6ee66be06cf9d961fafdce44
Gloria <ippnsj@gmail.com>
2021-11-15 03:23:49 +0900

    Fix bugs fix diary post api change emotion normal to none

--- src/utils/Emotions.js
+++ src/utils/Emotions.js
@@ -4,7 +4,7 @@ const SAD = {color:"#9FA7EF", tag:"슬픔", key:"sad"};
 const SURPRISED = {color:"#AEE477", tag:"놀람", key:"surprised"};
 const JEALOUS = {color:"#9431A4", tag:"질투", key:"jealous"};
 const FLUTTER = {color:"#F29CB6", tag:"설렘", key:"flutter"};
-const NORMAL = {color:"#FFFFFF", tag:"무난", key:"normal"};
+const NORMAL = {color:"#FFFFFF", tag:"무난", key:"none"};
 const TIRED = {color:"#ADADAD", tag:"지침", key:"tired"};
 const emotions = [HAPPY, ANGRY, SAD, SURPRISED, JEALOUS, FLUTTER, NORMAL, TIRED];
 
