21830957142ec20635dd13b6a06a7f62fe6507d0
Gloria <ippnsj@gmail.com>
2021-12-28 15:28:58 +0900

    Add motie add motie character and emotion

--- src/component/common/MotieFrame.js
+++ src/component/common/MotieFrame.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
+import character from '../../image/CharacterImg/dust.png'
+import eyes from '../../image/EmotionImg/11.png';
 
 function MotieFrame(props) {
     return (
-        <Frame></Frame>
+        <Frame>
+            <Motie character={character}>
+                <Eyes src={eyes}/>
+            </Motie>
+        </Frame>
     );
 }
 
@@ -11,4 +17,19 @@ export default MotieFrame;
 const Frame = styled.div`
     width: 100%;
     height: 100%;
+`
+
+const Motie = styled.div`
+    width: 100%;
+    height: 100%;
+    background: center / contain no-repeat url(${props => props.character});
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Eyes = styled.img`
+    max-width: 70%;
+    max-height: 70%;
 `
