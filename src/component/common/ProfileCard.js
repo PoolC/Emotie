245be5a71980d9e67d4c3d88672b908364614ddf
Gloria <ippnsj@gmail.com>
2022-01-21 16:26:40 +0900

    Modify ProfileCard remove height of profilecard container at moblie

--- src/component/common/ProfileCard.js
+++ src/component/common/ProfileCard.js
@@ -38,7 +38,6 @@ const Container = styled.div`
 
     @media only screen and (max-width: 768px) {
         width: 100%;
-        height: auto;
         aspect-ratio: 10/11;
         min-width: 300px;
         mix-height: 330px;
